c6b32a4a4f5e8a78dd42a9df8307e6438b1ff500
Nwaugojohnson <nwaugojohnson@gmail.com>
2025-08-08 10:58:28 +0100

    chore: project completed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import perfume from "./assets/images/image-product-mobile.jpg";
+import cart_icon from "./assets/images/icon-cart.svg";
+import PerfumeCard from "./components/PerfumeCard";
+
+function App() {
+  return (
+    <>
+    <PerfumeCard />
+    </>
+  );
+}
+
+export default App;
