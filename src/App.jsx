97f5562a0e4781571283fbac639de6e8073f91cc
Nwaugojohnson <nwaugojohnson@gmail.com>
2025-08-19 12:19:33 +0100

    features: changes made

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Icon from "./assets/images/icon-cart.svg";
 
 const App = () => {
   return (
-    <section className='font-fraunces'>
+    <section className='description'>
       <div className="min-h-screen bg-[#f2ebe3] flex items-center justify-center p-4">
       {/* Desktop Layout */}
       <div className="hidden md:flex max-w-2xl bg-white rounded-xl overflow-hidden">
@@ -16,10 +16,10 @@ const App = () => {
         {/* Product Info Section */}
         <div className="w-1/2 p-8 flex flex-col justify-between">
           <div>
-            <h2 className="text-xs font-fraunces text-[#6c7289] font-[500] tracking-[0.6em] mb-4">
+            <h2 className="text-xs font-fraunces text-[#6c7289] font-[700] tracking-[0.6em] mb-4">
               PERFUME
             </h2>
-            <h1 className="description text-5xl mb-6 font-[700] font-montserrat">
+            <h1 className="text-4xl mb-6 font-[700] font-montserrat">
               Gabrielle Essence Eau De Parfum
             </h1>
             <p className="description text-lg font-Fraunces font-[700] mb-6 text-[#6c7289] leading-relaxed ">
@@ -33,7 +33,7 @@ const App = () => {
               <p className="text-4xl mr-4 font-[700] font-montserrat text-[#3c8067]">$149.99</p>
               <p className="text-sm font-fraunces font-[500] line-through">$169.99</p>
             </div>
-            <button className="description w-full bg-[#3c8067] cursor-pointer hover:bg-[#1b4133] text-white font-fraunces font-[700] py-3 px-4 rounded-lg flex items-center justify-center transition-colors">
+            <button className="w-full bg-[#3c8067] cursor-pointer hover:bg-[#1b4133] text-white font-fraunces font-[700] py-3 px-4 rounded-lg flex items-center justify-center transition-colors">
               <img src={Icon} alt="" className="w-4 mr-2" />
               Add to Cart
             </button>
@@ -55,7 +55,7 @@ const App = () => {
           <h1 className="text-[2.2rem] font-[700] mb-4 font-montserrat">
             Gabrielle Essence Eau De Parfum
           </h1>
-          <p className="text-[18px] text-[#6c7289] font-[700] mb-6 leading-relaxed font-fraunces">
+          <p className="text-[20px] text-[#6c7289] font-[700] mb-6 leading-relaxed font-fraunces">
             A floral, solar and voluptuous interpretation composed by Olivier
             Polge, Perfumer-Creator for the House of CHANEL.
           </p>
